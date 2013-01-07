5764f58f32fc573391a18e6d3fbf5174742f027d
Sharan Kumar Monikanta Rajan <sharan.monikantan@gmail.com>
2013-01-08 03:55:31 +0530

    Completed the database functions

--- modules/drizzle.py
+++ modules/drizzle.py
@@ -30,16 +30,22 @@ try:
 except ImportError:
     has_mysqldb = False
 
-
 # Salt Dictionaries
 __outputter__ = {
     'ping': 'txt',
     'status': 'yaml',
-    'version': 'yaml'
+    'version': 'yaml',
+    'schemas': 'yaml',
+    'schema_exists': 'txt',
+    'schema_create': 'txt',
+    'schema_drop': 'txt',
+    'tables': 'yaml',
+    'table_find': 'yaml'
 }
 __opts__ = __salt__['test.get_opts']()
 
 
+# Check for loading the module
 def __virtual__():
     '''
     This module is loaded only if the
@@ -74,7 +80,6 @@ def _connect(**dsn):
             dsn_url[param] = dsn[param]
         else:
             dsn_url[param] = __opts__['drizzle.{0}'.format(param)]
-
     # Connecting to Drizzle!
     drizzle_db = MySQLdb.connect(**dsn_url)
     drizzle_db.autocommit(True)
@@ -94,10 +99,10 @@ def status():
 
     # Initializing the required variables
     ret_val = {}
-
-    # Fetching status
     drizzle_db = _connect()
     cursor = drizzle_db.cursor()
+
+    # Fetching status
     cursor.execute('SHOW STATUS')
     for iter in range(cursor.rowcount):
         status = cursor.fetchone()
@@ -106,6 +111,7 @@ def status():
     cursor.close()
     drizzle_db.close()
     return ret_val
+
 
 def version():
     '''
@@ -130,6 +136,159 @@ def version():
 
 
 # Database functions
+def schemas():
+    '''
+    Displays the schemas which are already
+    present in the Drizzle server
+
+    CLI Example::
+
+        salt '*' drizzle.schemas
+    '''
+    
+    # Initializing the required variables
+    ret_val = {}
+    drizzle_db = _connect()
+    cursor = drizzle_db.cursor()
+
+    # Retriving the list of schemas
+    cursor.execute('SHOW SCHEMAS')
+    for iter,count in zip(range(cursor.rowcount),range(1,cursor.rowcount+1)):
+        schema = cursor.fetchone()
+        ret_val[count] = schema[0]
+    
+    cursor.close()
+    drizzle_db.close()
+    return ret_val
+
+
+def schema_exists(schema):
+    '''
+    This method is used to find out whether
+    the given schema already exists or not
+
+    CLI Example::
+
+        salt '*' drizzle.schema_exists
+    '''
+
+    drizzle_db = _connect()
+    cursor = drizzle_db.cursor()
+
+    # Checking for existance
+    cursor.execute('SHOW SCHEMAS LIKE "{0}"'.format(schema))
+    cursor.fetchall()
+    if cursor.rowcount == 1:
+        return True
+    return False
+    
+
+def schema_create(schema):
+    '''
+    This method is used to create a schema. 
+    It takes the name of the schema as argument
+
+    CLI Example::
+
+        salt '*' drizzle.schema_create schema_name
+    '''
+
+    drizzle_db = _connect()
+    cursor = drizzle_db.cursor()
+
+    # Creating schema
+    try:
+        cursor.execute('CREATE SCHEMA {0}'.format(schema))
+    except MySQLdb.ProgrammingError:
+        return 'Schema already exists'
+
+    cursor.close()
+    drizzle_db.close()
+    return True
+
+
+def schema_drop(schema):
+    '''
+    This method is used to drop a schema.
+    It takes the name of the schema as argument.
+
+    CLI Example::
+
+        salt '*' drizzle.schema_drop schema_name
+    '''
+    
+    drizzle_db = _connect()
+    cursor = drizzle_db.cursor()
+
+    # Dropping schema
+    try:
+        cursor.execute('DROP SCHEMA {0}'.format(schema))
+    except MySQLdb.OperationalError:
+        return 'Schema does not exist'
+    
+    cursor.close()
+    drizzle_db.close()
+    return True
+
+
+def tables(schema):
+    '''
+    Displays all the tables that are
+    present in the given schema
+
+    CLI Example::
+
+        salt '*' drizzle.tables schema_name
+    '''
+
+    # Initializing the required variables
+    ret_val = {}
+    drizzle_db = _connect()
+    cursor = drizzle_db.cursor()
+
+    # Fetching tables
+    try:
+        cursor.execute('SHOW TABLES IN {0}'.format(schema))
+    except MySQLdb.OperationalError:
+        return 'Unknown Schema'
+
+    for iter,count in zip(range(cursor.rowcount),range(1,cursor.rowcount+1)):
+        table = cursor.fetchone()
+        ret_val[count] = table[0]
+
+    cursor.close()
+    drizzle_db.close()
+    return ret_val
+
+
+def table_find(table_to_find):
+    '''
+    Finds the schema in which the 
+    given table is present
+
+    CLI Example::
+
+        salt '*' drizzle.table_find table_name
+    '''
+
+    # Initializing the required variables
+    ret_val = {}
+    count = 1
+    drizzle_db = _connect()
+    cursor = drizzle_db.cursor()
+
+    # Finding the schema
+    schema = schemas()
+    for schema_iter in schema.iterkeys():
+        table = tables(schema[schema_iter])
+        for table_iter in table.iterkeys():
+            if table[table_iter] == table_to_find:
+                ret_val[count] = schema[schema_iter]
+                count = count+1
+
+    cursor.close()
+    drizzle_db.close()
+    return ret_val
 
 
 def ping():
