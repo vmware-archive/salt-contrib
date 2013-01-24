05bcd8ac09d9cc3d9023a83f93edc0f5b6ad1ca2
Toby Sears <admin@tobysears.co.uk>
2013-01-24 15:48:55 +0100

    Formatting change.
    - Changed tabs to 4 spaces.
    - Brought code better in line with Salt coding style.

--- modules/vzctl.py
+++ modules/vzctl.py
@@ -1,163 +1,187 @@
-"""
-Salt module to manage openvz hosts through vzctl and vzlist
-"""
+'''
+Salt module to manage openvz hosts through vzctl and vzlist.
+'''
 
 import salt.utils
 
 __outputter__ = {
-  'version': 'txt',
-  'list': 'txt',
-  'execute': 'txt',
-  'start': 'txt',
-  'stop': 'txt',
-  'restart': 'txt'
-}
+                'version': 'txt',
+                'list': 'txt',
+                'execute': 'txt',
+                'start': 'txt',
+                'stop': 'txt',
+                'restart': 'txt'
+                }
 
 def __virtual__():
-	'''
-Check to see if vzctl and vzlist are installed and load module
-'''
-	if salt.utils.which('vzctl') and salt.utils.which('vzlist'):
-		return 'vzctl'
-	return False
+    '''
+    Check to see if vzctl and vzlist are installed and load module
+    '''
+    if salt.utils.which('vzctl') and salt.utils.which('vzlist'):
+        return 'vzctl'
+    return False
 
 def version():
-	'''
-Return version from vzctl --version
+    '''
+    Return version from vzctl --version
 
-CLI Example::
+    CLI Example::
 
-	salt '*' vzctl.version
-''' 
-	out = __salt__['cmd.run']('vzctl --version')
-	return out
+    salt '*' vzctl.version
+    ''' 
+    out = __salt__['cmd.run']('vzctl --version')
+    return out
 
 def list():
-	'''
-Return list of containers from "vzlist -a"
+    '''
+    Return list of containers from "vzlist -a"
 
-CLI Example::
+    CLI Example::
 
-	salt '*' vzctl.list
-'''
-	out = __salt__['cmd.run']('vzlist -a')
-	return out
+    salt '*' vzctl.list
+    '''
+    out = __salt__['cmd.run']('vzlist -a')
+    return out
 
-def execute(ctid=None,option=None):
-	'''
-Execute a command on a container.
+def execute(ctid=None,
+          option=None):
+    '''
+    Execute a command on a container.
 
-CLI Example::
+    CLI Example::
 
-	salt '*' vzctl.execute 123 "df -h"
-'''
-	if not ctid:
-		return "Error: No container ID specified."
-	if not option:
-		return "Error: No option parameter specified."
+    salt '*' vzctl.execute 123 "df -h"
+    '''
+    if not ctid:
+        return "Error: No container ID specified."
+    if not option:
+        return "Error: No option parameter specified."
 	
-	ret, error = _checkCtid(ctid)
+    ret, error = _checkCtid(ctid)
 
-	if ret:
-		output = _runCommand("exec",ctid,option)
-		return output
-	else:
-		return error
+    if ret:
+        output = _runCommand(
+                            "exec",
+                            ctid,
+                            option
+                            )
+        return output
+    else:
+        return error
 
-def start(ctid=None,option=None):
-	'''
-Start a container.
+def start(ctid=None,
+        option=None):
+    '''
+    Start a container.
 
-CLI Example::
+    CLI Example::
 
-	salt '*' vzctl.start 123
+    salt '*' vzctl.start 123
 
-Can accept the --wait or --force arguments.
+    Can accept the wait or force arguments.
 
-For example::
+    For example::
 
-	salt '*' vzctl.start 123 --force
-'''
-	if not ctid:
-		return "Error: No container ID specified."
+    salt '*' vzctl.start 123 force
+    '''
+    if not ctid:
+        return "Error: No container ID specified."
 	
-	ret, error = _checkCtid(ctid)
+    ret, error = _checkCtid(ctid)
 
-	if ret:
-		output = _runCommand("start",ctid,option)
-		return output
-	else:
-		return error
+    if ret:
+        output = _runCommand(
+                             "start",
+                             ctid,
+                             option
+                             )
+        return output
+    else:
+        return error
 
-def stop(ctid=None,option=None):
-	'''
-Stop a container.
+def stop(ctid=None,
+         option=None):
+    '''
+    Stop a container.
 
-CLI Example::
+    CLI Example::
 
-	salt '*' vzctl.stop 123
+    salt '*' vzctl.stop 123
 
-Can accept the --wait or --skip-unmount arguments.
+    Can accept the wait or skip-unmount arguments.
 
-For example::
+    For example::
 
-	salt '*' vzctl.stop 123 --skip-unmount
-'''
-	if not ctid:
-		return "Error: No container ID specified."
+    salt '*' vzctl.stop 123 skip-unmount
+    '''
+    if not ctid:
+        return "Error: No container ID specified."
 	
-	ret, error = _checkCtid(ctid)
+    ret, error = _checkCtid(ctid)
 
-	if ret:
-		output = _runCommand("stop",ctid,option)
-		return output
-	else:
-		return error
+    if ret:
+        output = _runCommand(
+                            "stop",
+                            ctid,
+                            option
+                            )
+        return output
+    else:
+        return error
 
-def restart(ctid=None,option=None):
-	'''
-Restart a container.
+def restart(ctid=None,
+            option=None):
+    '''
+    Restart a container.
 
-CLI Example::
+    CLI Example::
 
-	salt '*' vzctl.restart 123
+    salt '*' vzctl.restart 123
 
-Can accept the --wait, --force or --fast arguments.
+    Can accept the wait, force or fast arguments.
 
-For example::
+    For example::
 
-	salt '*' vzctl.restart 123 --fast
-'''
-	if not ctid:
-		return "Error: No container ID specified."
-	
-	ret, error = _checkCtid(ctid)
+    salt '*' vzctl.restart 123 fast
+    '''
+    if not ctid:
+        return "Error: No container ID specified."
 
-	if ret:
-		output = _runCommand("restart",ctid,option)
-		return output
-	else:
-		return error
+    ret, error = _checkCtid(ctid)
+
+    if ret:
+        output = _runCommand(
+                            "restart",
+                            ctid,
+                            option
+                            )
+        return output
+    else:
+        return error
 
 def _checkCtid(ctid):
-	'''
-Checks to see if the ctid is a valid number
-'''
-	try:
-		ctid = int(ctid)
-		return True, None
-	except:
-		return False, "Error: ctid is not a number."
+    '''
+    Checks to see if the ctid is a valid number
+    '''
+    try:
+        ctid = int(ctid)
+        return True, None
+    except:
+        return False, "Error: ctid is not a number."
 
-def _runCommand(command,ctid,option):
-	'''
-Use salt to run the command and output.
-'''
-	if option is None:
-		cmd = 'vzctl %s %s' %(command,ctid)
-		out = __salt__['cmd.run'](cmd)
-		return out
-	else:
-		cmd = 'vzctl %s %s %s' %(command,ctid,option)
-		out = __salt__['cmd.run'](cmd)
-		return out
+def _runCommand(
+               command,
+               ctid,
+               option
+               ):
+    '''
+    Use salt to run the command and output.
+    '''
+    if option is None:
+        cmd = 'vzctl {0} {1}'.format(command,ctid)
+        out = __salt__['cmd.run'](cmd)
+        return out
+    else:
+        cmd = 'vzctl {0} {1} --{2}'.format(command,ctid,option)
+        out = __salt__['cmd.run'](cmd)
+        return out
