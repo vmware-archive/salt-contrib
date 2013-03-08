2645ac4e9adc25f6c670f88fa41b58493100dead
Tris Forster <tris.git@tfconsulting.com.au>
2013-03-08 14:01:33 +1100

    Adding sysmod and destructive tests

--- tests/contrib.py
+++ tests/contrib.py
@@ -2,18 +2,26 @@ from saltunittest import TestSuite, TestLoader
 import os
 
 tests = TestSuite()
+loader = TestLoader()
 
-current_dir = os.path.realpath(os.path.dirname(__file__))
+# add some useful tests from the main suite
+extra = ('integration.modules.sysmod', )
+tests.addTest(loader.loadTestsFromNames(extra))
+
+# this should resolve to the salt-contrib directory
+current_dir = os.path.dirname(os.path.realpath(__file__))
 l = len(current_dir)
 
 names = []
 for dirname, dirs, files in os.walk(current_dir):
     parts = dirname[l:].split(os.sep)
-    module = '.'.join(parts[1:])
+    if len(parts) < 2:
+        continue
 
+    module = '.'.join(parts[1:])
     for f in files:
-        if f[-8:] == '_test.py':
+        if f[-3:] == '.py':
             names.append('{0}.{1}'.format(module, f[:-3]))
 
-loader = TestLoader()
+print names
 tests.addTest(loader.loadTestsFromNames(names))
