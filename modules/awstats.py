d904c43fb617747a843c8116e338ac72818345ee
blambert555 <brent@enpraxis.net>
2013-05-16 15:14:40 -0600

    Added refactored awstats code, and added bacula File Daemon configuration state.

--- modules/awstats.py
+++ modules/awstats.py
@@ -1,51 +1,26 @@
 '''
-AWStats Configuration and Update for Static Deployment
+A module for managing awstats web statsicics in static mode
 
 :maintainer: Brent Lambert <brent@enpraxis.net>
 :maturity: new
 :platform: RedHat, Debian Families
+:depends: awstats cron
 
-A module to configure and update awstats deployed in non-dynamic mode, It
-
-- Controls access to statistics through basic authentication vi .htpasswd file
-- Adds and deletes users
-- Triggers an immediate update to current stats files managed by awstats.
-
-This module assumes that you already have your web server configured 
-with basic authentication, and that you already have a .htpasswd file
-present in the awstats static html directory (/var/www/awstats). You can 
-add these manually or set up salt states that provide the default 
-configuration. For an example how apache could be configured see the 
-example below (paths may need to be altered depending on your Linux 
-distribution)
-
-Example Apache configuration::
-
-  Alias /awstatscss /usr/share/awstats/wwwroot/css
-  Alias /awstatsicons /usr/share/awstats/wwwroot/icon
-  Alias /usage /var/www/awstats
-
-  <Directory /var/www/awstats>
-    AuthType Basic
-    AuthName "Web Site Statistics"
-    AuthUserFile /var/www/awstats/.htpasswd
-    Require valid-user
-    Options +FollowSymLinks
-  </Directory>
-
-Create empty password file::
-
-  touch /var/www/awstats/.htpasswd
+Assumes that the appropriate web server configuration and access
+controls have already been configured. Will generate a script for
+updating awstats static files, and will use it with cron to enable 
+automatic updates.
 
 '''
 
 from subprocess import Popen, PIPE
 import os
+import re
 
 
 # Script for updating awstats static pages
 
-update_script = '''#!/bin/bash
+awstats_update = '''#!/bin/bash
 SERVER={0}
 TARGET_DIR={1}
 BUILD_DATE=`date +"%Y-%m"`
@@ -59,39 +34,19 @@ fi
 ln -s $TARGET_DIR/awstats.$SERVER.$BUILD_DATE.html $TARGET_DIR/index.html
 '''
 
+# Default paths
+
+awstats_scr_path = '/usr/local/bin/awstats_update'
+awstats_static = '/usr/share/awstats/tools/awstats_buildstaticpages.pl'
+awstats_hourly = '/etc/cron.hourly/awstats'
+awstats_daily = '/etc/cron.daily/awstats'
+
 
 def __virtual__():
     '''
     Only supports RedHat and Debian OS Families for now
     '''
     return 'awstats' if __grains__['os_family'] in ['RedHat', 'Debian'] else False
-
-
-# Get Default locations for awstats stuff
-
-def _getAwstatsConfig():
-    '''
-    Get awstats scripts and configs based on OS Family
-    '''
-    config = { 'static':'/usr/share/awstats/tools/buildstaticpages.pl', }
-    if __grains__['os_family'] == 'RedHat':
-        config['awstats'] = '/usr/share/awstats/wwwroot/cgi-bin/awstats.pl'
-        config['model'] = '/etc/awstats/awstats.model.conf'
-    elif __grains__['os_family'] == 'Debian':
-        config['awstats'] = '/usr/lib/cgi-bin/awstats.pl'
-        config['model'] = '/etc/awstats/awstats.conf'
-    return config
-
-
-def _getTargetDir():
-    '''
-    Get configuration values based on OS Family
-    '''
-    if __grains__['os_family'] == 'RedHat':
-        return '/var/www/awstats'
-    elif __grains__['os_family'] == 'Debian':
-        return '/var/awstats'
-    return ''
 
 
 def _runcmd(cmd):
@@ -101,6 +56,18 @@ def _runcmd(cmd):
     child = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
     out, err = child.communicate()
     return child.returncode, out, err
+
+
+def _remove(fpath):
+    '''
+    If a file exist remove it
+    '''
+    try:
+        os.unlink(fpath)
+    except OSError, e:
+        # Ignore if file does not exist
+        if e.errno != 2:
+            raise OSError, e
 
 
 def configure(domain, logfile, period="hourly"):
@@ -120,91 +87,71 @@ def configure(domain, logfile, period="hourly"):
 
         salt 'server' awstats.configure domain=example.com \
           logfile=/var/log/nginx/access.log \
-          period=weekly
+          period=daily
 
         salt 'server' awstats.configure domain=example.com \
           logfile=/var/log/httpd/example.com-access_log
     '''
     if domain and logfile:
-        awcfg = _getAwstatsConfig()
-        tdir = _getTargetDir()
+
+        # Get OS specific values
+        if __grains__['os_family'] == 'RedHat':
+            awstats = '/usr/share/awstats/wwwroot/cgi-bin/awstats.pl'
+            model = '/etc/awstats/awstats.model.conf'
+            tdir = '/var/www/awstats'
+        elif __grains__['os_family'] == 'Debian':
+            awstats = '/usr/lib/cgi-bin/awstats.pl'
+            model = '/etc/awstats/awstats.conf'
+            tdir = '/var/awstats'
+        else:
+            return False
+
+        sd = re.compile('^SiteDomain=.*$', re.MULTILINE)
+        lf = re.compile('^LogFile=>*$', re.MULTILINE)
+        awconfig = '/etc/awstats/awstats.{0}.conf'.format(domain)
 
         # Generate a valid awstats configuration file
-        with open(awcfg['model']) as f:
+        with open(model) as f:
             config = f.read()
-            if __grains__['os_family'] == 'RedHat':
-                config = config.replace(
-                    'LogFile="/var/log/httpd/access_log"',
-                    'LogFile="{0}"'.format(logfile), 1)
-                config = config.replace(
-                    'SiteDomain="localhost.localdomain"',
-                    'SiteDomain="%s"' %domain, 1)
-            elif __grains__['os_family'] == 'Debian':
-                config = config.replace(
-                    'LogFile="/var/log/apache2/access.log"',
-                    'LogFile="{0}"'.format(logfile), 1)
-                config = config.replace(
-                    'SiteDomain=""',
-                    'SiteDomain="%s"' %domain, 1)
-            with open('/etc/awstats/awstats.%s.conf' %domain, 'w') as f1:
+            config = sd.sub('SiteDomain="{0}"'.format(domain), config)
+            config = lf.sub('LogFile="{0}"'.format(logfile), config)
+            with open(awconfig, 'w') as f1:
                 f1.write(config)
 
         # Generate an appropriate update script, store in /usr/local/bin
-        update = update_script.format(domain,
-                                      tdir,
-                                      awcfg['awstats'],
-                                      awcfg['static'])
-        with open('/usr/local/bin/awstats_update', 'w') as f:
-            os.chmod('/usr/local/bin/awstats_update', 0700)
+        update = awstats_update.format(domain,
+                                       tdir,
+                                       awstats,
+                                       awstats_static)
+
+        with open(awstats_scr_path, 'w') as f:
             f.write(update)
+            os.chmod(awstats_scr_path, 0700)
         
         # Set up cron
         if period == 'hourly':
-            if not os.path.exists('/etc/cron.hourly/awstats'):
-                os.symlink('/usr/local/bin/awstats_update', 
-                           '/etc/cron.hourly/awstats')
+            if not os.path.exists(awstats_hourly):
+                os.symlink(awstats_scr_path, awstats_hourly)
         elif period == 'daily':
-            if not os.path.exists('/etc/cron.daily/awstats'):
-                os.symlink('/usr/local/bin/awstats_update', 
-                           '/etc/cron.daily/awstats')
+            if not os.path.exists(awstats_daily):
+                os.symlink(awstats_scr_path, awstats_daily)
 
-        return domain,
+        return True
 
     return False
 
 
-def adduser(user, passwd):
+def disable():
     '''
-    Add a user and password to access awstats info
+    Disable automatic updates.
+
+    CLI_Example::
     
-    CLI_Example::
-
-        salt 'server' awstats.adduser bob test1234
+        salt 'server' awstats.disable
     '''
-    if user and passwd:
-        tdir = _getTargetDir()
-        cmd = '/usr/bin/htpasswd -b {0}/.htpasswd {1} {2}'.format(tdir, user, passwd)
-        result = _runcmd(cmd)
-        if not result[0]:
-            return True
-    return False
-
-
-def deleteuser(user):
-    '''
-    Delete user from password file, and prevent access to awstats info
-
-    CLI_Example::
-
-        salt 'server' awstats.deleteuser bob
-    '''
-    if user:
-        tdir = _getTargetDir()
-        cmd = '/usr/bin/htpasswd -D {0}/.htpasswd {1}'.format(tdir, user)
-        result = _runcmd(cmd)
-        if not result[0]:
-            return True
-    return False
+    _remove(awstats_hourly)
+    _remove(awstats_daily)
+    return True
 
 
 def update():
@@ -215,8 +162,9 @@ def update():
 
         salt 'server' awstats.update
     '''
-    result = _runcmd('/usr/local/bin/awstats_update')
+    result = _runcmd(awstats_scr_path)
     if result[0]:
+        # We have a return code, must be an error
         return False
     return True
     
