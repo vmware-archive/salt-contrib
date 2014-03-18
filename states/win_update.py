77acdb2182b6c8044a98519bc5970111da13386e
Narcolapser <sandslash@gmail.com>
2014-03-18 18:00:51 +0000

    I created a windows update class. The methods were getting a bit complex and there are far to many variables to take care of for me to want to do this with methods. so I decided to make a class to take care of all the variables needed for this theoretically simple task. (you know. like apt-get update; apt-get upgrade.)

--- states/win_update.py
+++ states/win_update.py
@@ -50,7 +50,110 @@ def _gather_update_categories(updateCollection):
 #       Security Updates
 #       Update Rollups
 
-def maintain(name,categories=None):
+class PyWinUpdater:
+        def __init__(self,skipUI = True,skipDownloaded = True,categories=None):
+                log.debug('CoInitializing the pycom system')
+                pythoncom.CoInitialize()
+                
+                self.skipUI = skipUI
+                self.skipDownloaded = skipDownloaded
+                self.categories = categories
+                self.soughtCategories = None
+                
+                log.debug('dispatching keeper to keep the session object.')
+                self.keeper = win32com.client.Dispatch('Microsoft.Update.Session')
+                
+                log.debug('keeper got. Now creating a seeker to seek out the updates')
+                self.seeker = self.session.CreateUpdateSearcher()
+                
+                #list of updates that are applicable by current settings.
+                self.quaffle = win32com.client.Dispatch('Microsoft.Update.UpdateColl')
+                
+                #list of updates to be installed.
+                self.bludger = win32com.client.Dispatch('Microsoft.Update.UpdateColl')
+                
+                #the object responsible for fetching the actual downloads. 
+                self.chaser = keeper.CreateUpdateDownloader()
+                self.chaser.Updates = quaffle
+                
+                #the object responsible for the installing of the updates.
+                self.beater = keeper.CreateUpdateInstaller()
+                self.beater.Updates = bludger
+                
+                self.points = None
+
+        def Search(self,searchString):
+                try:
+                        log.debug('beginning search of the passed string.')
+                        self.golden_snitch = self.seeker.Search(searchString)
+                        log.debug('search completed successfully.')
+                except Exception as e:
+                        log.info('search for updates failed. {0}'.format(str(e))
+                        return e
+                
+                log.debug('parsing results. {0} updates were found.'.format(
+                    str(self.golden_snitch.Updates.Count)))
+                try:
+                        for update in self.golden_snitch.Updates:
+                                if update.InstallationBehavior.CanRequestUserInput == True:
+                                        log.debug('Skipped update {0}'.format(str(update)))
+                                        continue
+                                for category in update.Categories:
+                                        if category.Name in self.categories or self.categories == None:
+                                                self.quaffle.Add(update)
+                                                log.debug('added update {0}'.format(str(update)))
+                        self.soughtCategories = _gather_update_categories(quaffle)
+                        return true
+                except Exception as e:
+                        log.info('parsing updats failed. {0}'.format(str(e))
+                        return e
+                        
+        def Download(self):
+                try:
+                        if self.quaffle.Count != 0:
+                                self.chaser.Download()
+                        else:
+                                log.debug('Skipped downloading, all updates were already cached.')
+                        return True
+                except Exception as e:
+                        log.debug('failed in the downloading {0}.'.format(str(e)))
+                        return e
+                
+        def Install(self):
+                try:
+                        for update in self.golden_snitch.Updates:
+                                if update.IsDownloaded:
+                                        self.bludger.Add(update)
+                        log.debug('Updates prepared. beginning installation')
+                except Exception as e:
+                        log.info('Preparing install list failed: {0}'.format(str(e)))
+                        return e
+                
+                if self.bludger.Count != 0:
+                        log.debug('Install list created, about to install')
+                        updates = []
+                        try:
+                                self.points = self.beater.Install()
+                                log.info('Installation of updates complete')
+                                return True
+                        except Exception as e:
+                                log.info('Installation failed: {0}'.format(str(e)))
+                                return e
+                else:
+                        log.info('no new updates.')
+                        return True
+        
+        def GetInstallationResults(self):
+                for i in range(self.bludger.Count):
+                        updates.append('{0}: {1}'.format(
+                                str(self.points.GetUpdateResult(i).ResultCode),
+                                str(self.bludger.Item(i).Title)))
+                results = {}
+                for i,update in enumerate(updates):
+                        results['update {0}'.format(i)] = update
+                return results
+
+def maintain(name,categories=None,retries=10):
         '''
         Set windows updates to run by category
 
@@ -72,89 +175,52 @@ def maintain(name,categories=None):
                'result': True,
                'changes': {},
                'comment': ''}
+        passed = False
+        while passed != True:
+                quidditch = PyWinUpdater()
+                passed = quidditch.Search('IsInstalled=0 and Type=\'Software\' and IsHidden=0')
+                if isinstance(passed,Exception):
+                        ret['comment'] += 'Failed in the seeking/parsing process:\n\t\t{0}\n'.format(str(passed))
+                        if retries:
+                                ret['comment'] += '{0} tries to go. retrying'.format(str(retries))
+                        else:
+                                ret['comment'] += 'out of retries. this update round failed.'
+                                ret['result'] = False
+                                return ret
+                        passed = False
+        
+        passed = False
+        
+        while not passed:
+                passed = quiddich.Download()
+                if isinstance(passed,Exception):
+                        ret['comment'] = 'Failed while trying to download updates:\n\t\t{0}'.format(str(passed))
+                        if retries:
+                                ret['comment'] += '{0} tries to go. retrying'.format(str(retries))
+                        else:
+                                ret['comment'] += 'out of retries. this update round failed.'
+                                ret['result'] = False
+                                return ret
+        
+        passed = False
+        
+        while not passed:
+                passed = quidditch.Install()
+                if isinstance(passed,Exception):
+                        ret['comment'] = 'Failed while trying to install the updates.\n\t\t{0}'.format(str(e))
+                        if retries:
+                                ret['comment'] += '{0} tries to go. retrying'.format(str(retries))
+                        else:
+                                ret['comment'] += 'out of retries. this update round failed.'
+                                ret['result'] = False
+                                return ret
         
         try:
-                log.debug('CoInitializing the pycom system')
-                pythoncom.CoInitialize()
-                
-                log.debug('dispatching keeper to keep the session object.')
-                keeper = win32com.client.Dispatch('Microsoft.Update.Session')
-                
-                log.debug('keeper got. Now creating a seeker to seek out the updates')
-                seeker = keeper.CreateUpdateSearcher()
-                
-                log.debug('seeker begining its seeking')
-                golden_snitch = seeker.Search('IsInstalled=0 and Type=\'Software\' and IsHidden=0')
-                
+                ret['changes'] = quidditch.GetInstallationResults()
         except Exception as e:
-                ret['comment'] = 'Failed in the seeking process:\n\t\t{0}'.format(str(e))
-                ret['result'] = False
-                return ret
-        
-        try: 
-                quaffle = win32com.client.Dispatch('Microsoft.Update.UpdateColl')
-                updates = []
-                log.debug('parsing results. {0} updates were found.'.format(str(golden_snitch.Updates.Count)))
-        
-                for update in golden_snitch.Updates:
-                        if update.InstallationBehavior.CanRequestUserInput == True:
-                                log.debug('Skipped update {0}'.format(str(update)))
-                                continue
-                        for category in update.Categories:
-                                if category.Name in categories or categories == None:
-                                        quaffle.Add(update)
-                                        log.debug('added update {0}'.format(str(update)))
-        except Exception as e:
-                ret['comment'] = 'Failed while parsing out the update:\n\t\t{0}'.format(str(e))
-                ret['result'] = False
-                return ret
-        
-        try:
-                categories = _gather_update_categories(quaffle)
-        
-        
-                if quaffle.Count != 0:
-                        chaser = keeper.CreateUpdateDownloader()
-                        chaser.Updates = quaffle
-                        chaser.Download()
-                else:
-                        ret['comment'] = 'Downloading was skipped as all updates were already cached.\n'
-                        log.debug('Skipped downloading, all updates were already cached.')
+                ret['comment'] += ['could not get results, but updates were installed']
                 
-        
-                bludger = win32com.client.Dispatch('Microsoft.Update.UpdateColl')
-        except Exception as e:
-                ret['comment'] = 'Failed while trying to download updates:\n\t\t{0}'.format(str(e))
-                ret['result'] = False
-                return ret
-        
-        try:
-                for i in range(golden_snitch.Updates.Count):
-                        update = golden_snitch.Updates.Item(i)
-                        if update.IsDownloaded:
-                                bludger.Add(update)
-                                updates.append(str(update))
-
-                if bludger.Count != 0:
-                        log.debug('Install list created, about to install')
-                        beater = keeper.CreateUpdateInstaller()
-                        beater.Updates = bludger
-                        points = beater.Install()
-                        for i in range(bludger.Count):
-                                updates.append('{0}: {1}'.format(
-                                        str(points.GetUpdateResult(i).ResultCode),
-                                        str(bludger.Item(i).Title)))
-                        log.info('Installation of updates complete')
-                        for i,update in enumerate(updates):
-                                ret['changes']['update {0}'.format(i)] = update
-                        return ret
-                log.info('Install complete, none were added as the system was already up to date.')
-                ret['comment'] += 'Now new updates. everything was already up to date'
-                return ret
-        except Exception as e:
-                ret['comment'] = 'Failed while trying to install the updates.\n\t\t{0}'.format(str(e))
-                ret['result'] = False
-                return ret
+        return ret
 
 ret = None
 
