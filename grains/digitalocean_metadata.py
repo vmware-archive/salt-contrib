a93eccf23427ae15028f3146c34a78316e99bc4a
David Boucha <boucha@gmail.com>
2014-10-24 00:43:27 -0600

    add grain data from DigitalOcean metadata server

---
+++ grains/digitalocean_metadata.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+'''
+    :codeauthor: David Boucha
+    :copyright: © 2014 by the SaltStack Team, see AUTHORS for more details.
+    :license: Apache 2.0, see LICENSE for more details.
+
+
+    salt.grains.digitalocean_metadata.py
+    ~~~~~~~~~~~~~~~~~~~~~~~
+
+    Create a DigitalOcean grain from the DigitalOcean metadata server.
+    See https://developers.digitalocean.com/metadata/#metadata-in-json
+
+    Note that not all datacenters were supported when this feature was first
+    released.
+'''
+
+# Import Python Libs
+import requests
+
+
+def digitalocean():
+    '''
+    Return DigitalOcean metadata.
+    '''
+    do_svr = 'http://169.254.169.254/metadata/v1.json'
+    metadata = requests.get(do_svr)
+
+    if metadata.status_code == 200:
+        return {'digitalocean': metadata.json()}
+    return {'digitalocean': []}
