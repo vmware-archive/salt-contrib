bb4a6cac3b14dd272ebeed16fb22b5d9b2a0563e
Mark McGuire <mark.b.mcg@gmail.com>
2015-01-11 21:30:33 -0600

    Improve ec2_info grain
    - unflatten the ec2 grains
    - snake case all aws keys
    - parse json values (for ami permissions)

--- grains/ec2_info.py
+++ grains/ec2_info.py
@@ -3,7 +3,7 @@
 """
 Get some grains information that is only available in Amazon AWS
 
-Author: Erik Günther, J C Lawrence <claw@kanga.nu>
+Author: Erik Günther, J C Lawrence <claw@kanga.nu>, Mark McGuire
 
 """
 import logging
@@ -28,7 +28,7 @@ def _call_aws(url):
         return response.read()
 
 
-def _get_ec2_hostinfo(path="", data={}):
+def _get_ec2_hostinfo(path=""):
     """
     Recursive function that walks the EC2 metadata available to each minion.
     :param path: URI fragment to append to /latest/meta-data/
@@ -37,15 +37,37 @@ def _get_ec2_hostinfo(path="", data={}):
     All EC2 variables are prefixed with "ec2_" so they are grouped as grains and to
     avoid collisions with other grain names.
     """
-    for line in _call_aws("/latest/meta-data/%s" % path).split("\n"):
+    resp = _call_aws("/latest/meta-data/%s" % path).strip()
+    d = {}
+    for line in resp.split("\n"):
         if line[-1] != "/":
             call_response = _call_aws("/latest/meta-data/%s" % (path + line))
             if call_response is not None:
-                data["ec2_" + path.replace("/", "_") + line] = call_response
+                line = _dash_to_snake_case(line)
+                try:
+                    data = json.loads(call_response)
+                    if isinstance(data, dict):
+                        data = _snake_caseify_dict(data)
+                    d[line] = data
+                except ValueError:
+                    d[line] = call_response
             else:
-                data["ec2_" + path.replace("/", "_")[:-1]] = line
+                return line
         else:
-            _get_ec2_hostinfo(path + line, data=data)
+            d[_dash_to_snake_case(line[:-1])] = _get_ec2_hostinfo(path + line)
+    return d
+
+
+def _camel_to_snake_case(s):
+    return s[0].lower() + "".join((("_" + x.lower()) if x.isupper() else x) for x in s[1:])
+
+
+def _dash_to_snake_case(s):
+    return s.replace("-", "_")
+
+
+def _snake_caseify_dict(d):
+    return {_camel_to_snake_case(k): v for k, v in d.items()}
 
 
 def _get_ec2_additional():
@@ -55,13 +77,8 @@ def _get_ec2_additional():
     architecture.
 
     """
-    data = _call_aws("/latest/dynamic/instance-identity/document")
-    rc = dict()
-    for pair in json.loads(data).items ():
-        # De-camelcase the keys: availabilityZone -> availability-zone
-        key = "".join((("-" + x.lower()) if x.isupper() else x) for x in pair[0])
-        rc["ec2_" + key] = pair[1]
-    return rc
+    data = json.loads(_call_aws("/latest/dynamic/instance-identity/document"))
+    return _snake_caseify_dict(data)
 
 
 def ec2_info():
@@ -77,8 +94,8 @@ def ec2_info():
 
     try:
         grains = _get_ec2_additional()
-        _get_ec2_hostinfo(data=grains)
-        return grains
+        grains.update(_get_ec2_hostinfo())
+        return {'ec2' : grains}
     except socket.timeout, serr:
         LOG.info("Could not read EC2 data (timeout): %s" % (serr))
         return {}
